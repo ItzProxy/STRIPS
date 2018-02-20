74f260c6726a4449bb2c5b7e00258e1cf280cf4f
Dionne Pasion <allure.success@gmail.com>
2018-02-20 14:45:47 -0600

    Made some more headway into understanding STRIPS AI

--- strips.py
+++ strips.py
@@ -1,3 +1,52 @@
+class GroundedCondition:
+    def __init__(self, predicate, literals, truth = True):
+        self.predicate = predicate
+        self.literals = literals
+        self.truth = truth
+
+    def reached(self, world):
+        return world.is_true(self.predicate, self.literals) == self.truth
+
+    def __str__(self):
+        name = self.predicate
+        if(not self.truth):
+            name = "!" + name
+        return "{0}({1})".format(name, join_list(self.literals))
+
+class Actions:
+    def __init__(self, name, params, preconditions, postconditions):
+        self.name = name
+        self.params = params
+        self.preconditions = preconditions
+        self.postconditions = postconditions
+    
+    def generate_groundings(self, world):
+        self.grounds = []
+        current_literals = []
+        self.groundings_helper(world.known_literals, current_literals, self.grounds)
+
+    def groundings_helper(self, all_literals, current_literals, grounds):
+        if(len(current_literals) == len(self.params)):
+            arg_map = dict(zip(self.params, current_literals))
+            grounded_precond = [p.ground(arg_map) for p in self.preconditions]
+            grounded_postcond = [p.ground(arg_map) for p in self.postconditions]
+            grounds.append(GroundedAction(self,current_literals, grounded_precond, grounded_postcond))
+            return
+        for(literal in all_literals):
+            if(literal not in current_literals):
+                self.groundings_helper(all_literals, current_literals + [literal], grounds)
+        
+        def print_grounds(self):
+            i = 0
+            for(g in self.grounds):
+                print("Grounding " + str(i))
+                print(grounds)
+                i+=1
+
+        def __str__(self):
+            return "{0}({1})\nPre: {2}\nPost: {3}".format(self.name, join_list(self.params), join_list(self.preconditions), join_list(self.postconditions))
+            
+
 class ParseState:
     INITIAL=1
     GOAL=2
@@ -26,6 +75,18 @@ class World:
 
     def is_false(self, predicate,literals):
         return not self.is_true(predicate,literals)
+
+    def set_true(self, predicate, literals):
+        if(predicate not in self.state):
+            self.state[predicate] = set()
+        self.state[predicate].add(literals)
+    
+    def set_false(self, predicate, literals):
+        if(predicate in self.state):
+            self.state[predicate].remove(literals)
+
+    def add_goal(self, predicate, literals, truth=True):
+        g = 
 
     def print(self):
         print("Hello there")
